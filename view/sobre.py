3243caf22e8bbd40b911bd33890cb17b21f8f961
Matheus_WD_2006 <matheusditz.pf028@academico.ifsul.edu.br>
2026-06-11 10:35:40 -0300

    o sobre foi arrumado

--- view/sobre.py
+++ view/sobre.py
@@ -6,13 +6,13 @@ class Sobre(tk.Toplevel):
     def __init__(self, parent):
         super().__init__(parent)
         self.title("Sobre")
-        self.geometry("400x300")
+        self.geometry("800x500")
         self.resizable(False, False)
         self.grab_set()
 
-        self._build()
+        self.construir()
 
-    def _build(self):
+    def construir(self):
         frame = tk.Frame(self, padx=20, pady=20)
         frame.place(relx=0.5, rely=0.5, anchor="center")
 
@@ -24,7 +24,16 @@ class Sobre(tk.Toplevel):
 
         tk.Label(
             frame,
-            text="Descrição do sistema aqui.",
+            text="Descrição: Este sistema foi desenvolvido para auxiliar agricultores " \
+            "e gestores agrícolas a monitorar e gerenciar suas culturas de forma eficiente. " \
+            "Ele oferece funcionalidades como cadastro de culturas, " \
+            "plantas disponíveis para plantio dependendo da época bem " \
+            "como ter um controle do que e quando foi plantado/colhido," \
+            " o que ajuda na hora das rotações de culturas." \
+            "Desenvolvi esse sistema com base em minha vivência própia, onde desde pequeno vivi em uma propriedade rural" \
+            " e sempre tive interesse em tecnologia," \
+            " então decidi unir as duas coisas e criar um sistema que pudesse ajudar outros" \
+            " agricultores a gerenciar suas culturas de forma mais eficiente.",
             font=("Arial", 10),
             wraplength=340,
             justify="center"
@@ -32,14 +41,9 @@ class Sobre(tk.Toplevel):
 
         tk.Label(
             frame,
-            text="Desenvolvido por:",
+            text="Desenvolvido por: Matheus Witte Ditz, " \
+            "estudante no 3° ano de Ciências da Computação no IFSUL de Passo Fundo RS.",
             font=("Arial", 10, "bold")
-        ).pack()
-
-        tk.Label(
-            frame,
-            text="Seu Nome Aqui",
-            font=("Arial", 10)
         ).pack()
 
         tk.Label(
